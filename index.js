333a633ed480ab0ae800f13695aa0fe16a04933e
Ramesh <gurams@gmail.com>
2017-10-15 21:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello, world!");
 
 var name = "Ram";
 var height = "70";
-module.exports{name,height}
+module.exports={name,height};
 
 if (name === "susan"){
   console.log("name is Susan");
