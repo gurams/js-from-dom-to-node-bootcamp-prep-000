47930f9d3dfc0b47a962f7421eb59e118a476783
Ramesh <gurams@gmail.com>
2017-10-15 21:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ console.log("Hello, world!");
 
 var name = "Ram";
 var height = "70";
-module.exports{name,height};
+module.exports{name,height}
 
 if (name === "susan"){
   console.log("name is Susan");
 }
 else {
-  console.log(" name is not susan");  
+  console.log(" name is not susan");
 }
